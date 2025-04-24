165cb5959f5d17f0a291fbe23cc2d79037bd2fb2
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-04-24 12:40:44 -0400

    Add Starship config file and install script

---
+++ install/starship.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source "../utils/log.sh"
+
+### Parse args ###
+dry_run="false"
+
+if [[ "${1:-}" == "--dry-run" ]]; then
+	dry_run="true"
+fi
+
+### Install Starship prompt ###
+log INFO "Installing Starship shell prompt..."
+if [[ "$dry_run" == "false" ]]; then
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
+log OK "Ghostty setup complete!\n"
