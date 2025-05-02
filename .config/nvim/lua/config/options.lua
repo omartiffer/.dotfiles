a77b7cef5ff753d78f5b433e34a144e471737e78
Omar Tiffer <oatiffer@gmail.com>
2025-05-02 01:39:06 -0400

    Split options and remaps to their own files

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,19 @@
+vim.g.loaded_netrw = 0
+vim.g.netrw_banner = 0
+vim.g.loaded_netrwPlugin = 0
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
---
+++ .config/nvim/lua/config/remaps.lua
@@ -0,0 +1,10 @@
+
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "[d", function() vim.diagnossic.get_prev() end, { desc = "Prev diagnostic" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.get_next() end, { desc = "Next diagnostic" })
+vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+vim.keymap.set("n", "<leader>sdl", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
