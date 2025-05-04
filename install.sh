1979788860a54e726ebc85c5a1046c8fcb66974d
Omar Tiffer <oatiffer@gmail.com>
2025-05-04 12:47:41 -0400

    Add license header to appropriate files

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+# Copyright 2025 Omar Tiffer
+#
+# Portions of this file are based on code from ThePrimeagen’s
+# "My Dev Setup Is Better Than Yours" course on Frontend Masters.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -euo pipefail
 trap 'echo -e "\nError on line $LINENO: $BASH_COMMAND"' ERR
 
 export DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$DOTFILES"/utils.sh
-
-readarray -t scripts < <(find "$DOTFILES"/installs -maxdepth 1 -mindepth 1 -type f)
 
 ### Parse args ###
 filter=""
@@ -24,16 +39,16 @@ done
 
 log INFO "Running with filter=$filter..."
 
-for script in "${scripts[@]}"; do
+while IFS= read -r script; do
     if echo "$script" | grep -qv "$filter"; then
-        log PLAIN "Filtered: $filter -- $script"
+        log PLAIN "Filtered: $script"
         continue
     fi
-    log WARN "Running script: $script..."
 
+    log WARN "Running: $script..."
     if [[ $dry_run == "true" ]]; then
         bash "$script" --dry-run
     else
         bash "$script"
     fi
-done
+done < <(find "$DOTFILES"/installs -maxdepth 1 -mindepth 1 -type f)
