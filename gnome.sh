2e89b6abe5a06dbd98fa0e2224bff616e255d1b1
Omar Tiffer <79622899+omartiffer@users.noreply.github.com>
2025-05-13 11:35:02 -0400

    Change gnome settings

--- gnome.sh
+++ gnome.sh
@@ -7,6 +7,11 @@ parse_args "$@"
 log INFO "Restoring GNOME background images..."
 if_not_dry find "$DOTFILES/gnome-files/backgrounds" -type f -exec cp -a {} "$HOME/.local/share/backgrounds/" \; >/dev/null
 
+log INFO "Setting wallpaper..."
+gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/Northern_lights_by_matthieugo.png"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/.local/share/backgrounds/Northern_lights_by_matthieugo.png"
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+
 log INFO "Loading GNOME config..."
 if_not_dry dconf load / <"$DOTFILES/gnome-files/gnome.dconf"
 
